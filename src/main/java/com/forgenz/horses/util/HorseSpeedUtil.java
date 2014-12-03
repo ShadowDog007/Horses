76998d996f1c6314f3de88a8a80924ed6147f7f1
Michael McKnight <michael@mcknight.ninja>
2014-12-03 19:13:38 +1300

    Updated Horses for 1.8

--- src/main/java/com/forgenz/horses/util/HorseSpeedUtil.java
+++ src/main/java/com/forgenz/horses/util/HorseSpeedUtil.java
@@ -71,7 +71,7 @@ public class HorseSpeedUtil
 			Class<?> genAttributes = null;
 			try
 			{
-				genAttributes = Class.forName("net.minecraft.server.v1_7_R4.GenericAttributes");
+				genAttributes = Class.forName("net.minecraft.server.v1_8_R1.GenericAttributes");
 			}
 			catch (Throwable e)
 			{
@@ -87,7 +87,7 @@ public class HorseSpeedUtil
 			getHandle = ClassUtil.getMethod(horse.getClass(), "getHandle");
 			
 			Object mcHorse = getHandle.invoke(horse); 
-			Class<?> iattribute = Class.forName("net.minecraft.server.v1_7_R4.IAttribute");
+			Class<?> iattribute = Class.forName("net.minecraft.server.v1_8_R1.IAttribute");
 			getAttributeInstance = ClassUtil.getMethod(mcHorse.getClass(), "getAttributeInstance", iattribute);
 			
 			Object attributeRanged = getAttributeInstance.invoke(mcHorse, speedAttribute);
